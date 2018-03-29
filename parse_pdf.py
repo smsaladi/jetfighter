eeeb01dbe83a3c6135bc79cf690802eee2d47609
Shyam Saladi <smsaladi@gmail.com>
2018-03-29 09:58:05 -0700

    initial parser to infer cmap used

--- parse_pdf.py
+++ parse_pdf.py
@@ -7,10 +7,17 @@ import argparse
 import tempfile
 import subprocess
 import glob
+import itertools
+
 
 import numpy as np
 import pandas as pd
+from sklearn.neighbors import NearestNeighbors
+
 import skimage.io
+import matplotlib.pyplot as plt
+from colorspacious import cspace_convert
+
 
 def convert_to_img(fn):
     with tempfile.TemporaryDirectory() as td:
@@ -46,6 +53,85 @@ def parse_img(fn):
     return col
 
 
+# Have colormaps separated into categories:
+# http://matplotlib.org/examples/color/colormaps_reference.html
+cmap_names = [('Perceptually Uniform Sequential', [
+            'viridis', 'plasma', 'inferno', 'magma']),
+         ('Sequential', [
+            'Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds',
+            'YlOrBr', 'YlOrRd', 'OrRd', 'PuRd', 'RdPu', 'BuPu',
+            'GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn', 'YlGn']),
+         ('Sequential (2)', [
+            'binary', 'gist_yarg', 'gist_gray', 'gray', 'bone', 'pink',
+            'spring', 'summer', 'autumn', 'winter', 'cool', 'Wistia',
+            'hot', 'afmhot', 'gist_heat', 'copper']),
+         ('Diverging', [
+            'PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu',
+            'RdYlBu', 'RdYlGn', 'Spectral', 'coolwarm', 'bwr', 'seismic']),
+         ('Qualitative', [
+            'Pastel1', 'Pastel2', 'Paired', 'Accent',
+            'Dark2', 'Set1', 'Set2', 'Set3',
+            'tab10', 'tab20', 'tab20b', 'tab20c']),
+         ('Miscellaneous', [
+            'flag', 'prism', 'ocean', 'gist_earth', 'terrain', 'gist_stern',
+            'gnuplot', 'gnuplot2', 'CMRmap', 'cubehelix', 'brg', 'hsv',
+            'gist_rainbow', 'rainbow', 'jet', 'nipy_spectral', 'gist_ncar'])]
+
+def build_cmap_knn(n=256):
+    # matplotlib.cm.ScalarMappable(cmap=plt.get_cmap('jet')).to_rgba([.1, 0.5, .9], alpha=False, bytes=True)
+    cmaps = {}
+    cm_names = [cat[1] for cat in cmap_names]
+    for name in itertools.chain.from_iterable(cm_names):
+        cm = plt.get_cmap(name)
+        cmaps[name] = cm(np.linspace(0, 1, n))[:,:3]
+        cmaps[name] = cspace_convert(cmaps[name], "sRGB1", "CAM02-UCS")
+        cmaps[name] = NearestNeighbors(n_neighbors=1, metric='euclidean').fit(cmaps[name])
+    return cmaps
+
+cmap_knn = build_cmap_knn()
+
+
+def convert_to_jab(df):
+    arr_tmp = cspace_convert(df[['R', 'G', 'B']], "sRGB255", "CAM02-UCS")
+    df[['J', 'a', 'b']] = pd.DataFrame(arr_tmp, index=df.index)
+
+    df.drop(columns=['R', 'G', 'B'], inplace=True)
+    return df
+
+
+def find_cm_dists(df, max_diff=1.0):
+    """Expects counts of colors in jab format
+
+    find closest color in cm (in jab space):
+    if diff < max_diff:
+        then assume thats the mapping
+        calculate difference with each remaining page color
+        if less than `max_diff`,
+            then assume they correspond
+    calculate a % of colormap accounted data,
+                % of data accounted for by colormap
+    """
+
+    cm_stats = pd.DataFrame(index=cmap_knn.keys(),
+                            columns=['pct_cm', 'pct_page'])
+    cm_stats.index.name = 'cm'
+
+    for cm_name, cm_knn in cmap_knn.items():
+        dist, idx = cm_knn.kneighbors(df[['J', 'a', 'b']])
+
+        idx = idx[dist < max_diff]
+        dist = dist[dist < max_diff]
+
+        cm_colors = np.unique(idx)
+
+        cm_stats.loc[cm_name, ['pct_cm', 'pct_page']] = [
+            cm_colors.size / 256,
+            idx.size / df.shape[0]
+        ]
+
+    return cm_stats
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('pdf_file')
@@ -55,8 +141,15 @@ def main():
     df = pd.concat([parse_img(p) for p in convert_to_img(args.pdf_file)],
         ignore_index=True, copy=False)
 
+    # Write out RGB colors found
     name, _ = os.path.splitext(args.pdf_file)
     df.to_csv(name + '_colors.csv', index=False)
+
+
+    # Find nearest color for each page
+    df = convert_to_jab(df)
+    df_cmap = df.groupby('fn').apply(find_cm_dists)
+    df_cmap.to_csv(name + '_cm.csv')
 
     return
 
