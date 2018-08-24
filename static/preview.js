8f88e99031bc6398b67c30b75d978ba8770a83bc
Shyam Saladi <smsaladi@gmail.com>
2018-08-24 00:50:25 -0700

    slicker loading wheel/img handling. muse jquery for everything

--- static/preview.js
+++ static/preview.js
@@ -1,52 +1,49 @@
 // Initiate conversion of images.
 // Separate call for which pages and retrieving images
 function retrieve_previews(id, all_pages = false) {
-  info = $('#infotemplate').clone()[0];
-  info.removeAttribute('id');
-  p_preview = $(info).find(".paperpreview")[0];
+  info = $('#infotemplate').clone();
+  info.removeAttr('id');
+  p_preview = info.find(".paperpreview");
 
-  detail_link = $(info).find(".detail_link");
+  detail_link = info.find(".detail_link");
   detail_link.attr("href", '/detail/' + id);
-
-  // Retrieve image numbers along with per-page parse statuses
-  // add placeholders on the page until finally retrieved
-  function insertPage(id, pg, i) {
-      i.src = '';
-      i.className += " fa fa-circle-notch fa-spin";
-      function replacePagePlaceholder(data) {
-        i.src = data;
-        i.classList.remove('fa');
-        i.classList.remove('fa-circle-notch');
-        i.classList.remove('fa-spin');
-      }
-      $.ajax({
-        url : "/preview/" + id + "/" + pg,
-        dataType: 'json',
-      }).done(replacePagePlaceholder);
-  }
 
   function insertPages(pgs) {
     Object.keys(pgs).map(function(pg, index) {
       status = pgs[pg];
-      pvt = $('#pgpreviewtempl').clone()[0];
-      pvt.removeAttribute('id');
+      pvt = $('#pgpreviewtempl').clone();
+      pvt.removeAttr('id');
 
       // add link to page in pdf
-      a_link = $(pvt).find("a");
+      a_link = pvt.find("a");
       a_link.attr("href",
         'http://biorxiv.org/cgi/content/short/' + id + '.pdf#page=' + pg
       );
 
       // call to retrieve page and insert it
-      p_preview.appendChild(pvt);
-      i = $(pvt).find("img")[0];
+      p_preview.append(pvt);
 
-      if (status == "true") {
-        i.classList.add("img-preview-detected");
-      } else {
-        i.classList.add("img-preview-notdetected");
-      }
-      insertPage(id, pg, i);
+      if (status == "true")
+        pvt.find("a").children().addClass("preview-detected");
+      else
+        pvt.find("a").children().addClass("preview-notdetected");
+
+      // Retrieve image numbers along with per-page parse statuses
+      // remove placeholders
+      i = pvt.find("img");
+      i.hide();
+      i.attr("src", "");
+      $.ajax({
+        url : "/preview/" + id + "/" + pg,
+        dataType: 'json',
+      }).done((function( pvt, i ){
+        return function(data) {
+          pvt.find(".placeholder").remove();
+          i.attr("src", data);
+          i.show();
+        };
+      })( pvt, i ));
+
     });
   }
 
